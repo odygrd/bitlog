46aaa3ea0ea007b15f867e77928fb179fb38cbba
Odysseas Georgoudis <odygrd@gmail.com>
2024-08-23 16:52:27 +0100

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,1 +1,40 @@
-#include <filesystem>#include <iostream>#include <system_error>#include <thread>#include "bitlog/backend.h"#include "bitlog/bitlog.h"int main(){  using frontend_options_t =    bitlog::FrontendOptions<bitlog::QueuePolicyOption::BoundedBlocking, bitlog::QueueTypeOption::Default, true>;  using frontend_t = bitlog::Frontend<frontend_options_t>;  frontend_options_t frontend_options{};  frontend_t::init("test_app", frontend_options);  bitlog::SinkOptions sink_options;  sink_options.output_file_suffix(bitlog::FileSuffix::StartDateTime);  auto fsink = frontend_t::instance().create_file_sink(    "/home/odygrd/CLionProjects/bitlog/output/test_file.log", sink_options);  auto logger = frontend_t::instance().create_logger("testing", fsink);  LOG_INFO(logger, "hello world {}", 12u);  LOG_INFO(logger, "hello doubles {}", 123.3);  auto logger_2 = frontend_t::instance().create_logger("another_logger", fsink);  for (int i = 0; i < 3; ++i)  {    LOG_INFO(logger_2, "hello char {} loop {}", i * 100, i);  }  LOG_INFO(logger, "Another log doubles {}", 123.3);  std::this_thread::sleep_for(std::chrono::seconds{30});  return 0;}
+#include <filesystem>
+#include <iostream>
+#include <system_error>
+#include <thread>
+
+#include "bitlog/backend.h"
+#include "bitlog/bitlog.h"
+
+int main()
+{
+  using frontend_options_t =
+    bitlog::FrontendOptions<bitlog::QueuePolicyOption::BoundedBlocking, bitlog::QueueTypeOption::Default, true>;
+  using frontend_t = bitlog::Frontend<frontend_options_t>;
+
+  frontend_options_t frontend_options{};
+
+  frontend_t::init("test_app", frontend_options);
+
+  bitlog::SinkOptions sink_options;
+  sink_options.output_file_suffix(bitlog::FileSuffix::StartDateTime);
+  auto fsink = frontend_t::instance().create_file_sink(
+    "/home/odygrd/CLionProjects/bitlog/output/test_file.log", sink_options);
+
+  auto logger = frontend_t::instance().create_logger("testing", fsink);
+
+  LOG_INFO(logger, "hello world {}", 12u);
+  LOG_INFO(logger, "hello doubles {}", 123.3);
+
+  auto logger_2 = frontend_t::instance().create_logger("another_logger", fsink);
+
+  for (int i = 0; i < 3; ++i)
+  {
+    LOG_INFO(logger_2, "hello char {} loop {}", i * 100, i);
+  }
+
+  LOG_INFO(logger, "Another log doubles {}", 123.3);
+
+  std::this_thread::sleep_for(std::chrono::seconds{30});
+  return 0;
+}
